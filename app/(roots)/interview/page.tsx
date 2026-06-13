c623a9914e79dbb621900bd199bff11aeb9ed46f
aadityasinghrana06-dot <aadityasinghrana06@gmail.com>
2026-06-14 05:26:13 +0530

    fixed constants and project errors

--- app/(roots)/interview/page.tsx
+++ app/(roots)/interview/page.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+// import React from 'react'
+// import Agent from "@/components/agent";
+// import {awaitExpression} from "@babel/types";
+// import {getCurrentUser} from "@/lib/actions/auth.action";
+//
+// const Page = () => {
+//     const Page = async () => {
+//         const user = await getCurrentUser();
+//
+//
+//         return (
+//             <>
+//                 <h3>Interview Generation</h3>
+//
+//                 <Agent userName="you" userId="user1" type="generate"/>
+//             </>
+//         )
+//     }
+//         export default Page;
 import Agent from "@/components/agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 
-const Page = () => {
+const Page = async () => {
+    const user = await getCurrentUser();
+
     return (
         <>
-           <h3>Interview Generation</h3>
-
-            <Agent userName="you" userId="user1" type="generate" />
+            <h3>Interview generation</h3>
+            <Agent userName={user?.name || "User"} />
         </>
-    )
-}
-export default Page
+    );
+};
+
+export default Page;
