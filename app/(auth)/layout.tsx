aa701547a6c5c323fc6a3b8fa8a2a4f3987274c4
aadityasinghrana06-dot <aadityasinghrana06@gmail.com>
2026-06-14 00:55:14 +0530

    Fix auth redirect

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,14 +1,27 @@
-import {ReactNode} from 'react'
-import {redirect} from "next/navigation";
-import {isAuthenticated} from "@/lib/actions/auth.action";
+// import {ReactNode} from 'react'
+// import {redirect} from "next/navigation";
+// import {isAuthenticated} from "@/lib/actions/auth.action";
+//
+// const AuthLayout = async ({children}: {children:ReactNode }) => {
+//     const isUserAuthenticated = await isAuthenticated();
+//
+//     if(!isUserAuthenticated) redirect('/');
+//
+//     return (
+//         <div className="auth-layout">{children}</div>
+//     )
+// }
+// export default AuthLayout
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
+import { isAuthenticated } from "@/lib/actions/auth.action";
 
-const AuthLayout = async ({children}: {children:ReactNode }) => {
+const AuthLayout = async ({ children }: { children: ReactNode }) => {
     const isUserAuthenticated = await isAuthenticated();
 
-    if(!isUserAuthenticated) redirect('/');
+    if (isUserAuthenticated) redirect("/");
 
-    return (
-        <div className="auth-layout">{children}</div>
-    )
-}
-export default AuthLayout
+    return <>{children}</>;
+};
+
+export default AuthLayout;
