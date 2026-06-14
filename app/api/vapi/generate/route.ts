0f80ec545da310c68d9962d4b808798ef13e7186
aadityasinghrana06-dot <aadityasinghrana06@gmail.com>
2026-06-14 05:41:15 +0530

    fix vapi generate route

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -30,20 +30,22 @@
 // }
 import { generateText } from "ai";
 import { google } from "@ai-sdk/google";
-import {getRandomInterviewCover} from "@/lib/utils";
-import {db} from "@/firebase/admin";
+import { getRandomInterviewCover } from "@/lib/utils";
+import { db } from "@/firebase/admin";
 
-return Response.json(
-    {
-        success: true,
-        questions: JSON.parse(questions),
-    },
-    { status: 200 }
-);
+export async function GET() {
+    return Response.json(
+        {
+            success: true,
+            data: "THANK YOU",
+        },
+        { status: 200 }
+    );
 }
 
 export async function POST(request: Request) {
-    const { type, role, level, techstack, amount, userid } = await request.json();
+    const { type, role, level, techstack, amount, userid } =
+        await request.json();
 
     try {
         const { text: questions } = await generateText({
@@ -61,25 +63,23 @@ The questions are going to be read by a voice assistant so do not use "/" or "*"
 
 Return the questions formatted like this:
 ["Question 1", "Question 2", "Question 3"]`,
-
         });
 
-
         const interview = {
-            role, type, level,
-            techstack: techstack.split(','),
+            role,
+            type,
+            level,
+            techstack: techstack.split(","),
             questions: JSON.parse(questions),
             userId: userid,
             finalized: true,
             coverImage: getRandomInterviewCover(),
             createdAt: new Date().toISOString(),
-        }
-         await db.collection("interviews").add(interview);
-        return Response.json(
-            { success: true},
-            { status: 200 }
-        );
+        };
 
+        await db.collection("interviews").add(interview);
+
+        return Response.json({ success: true }, { status: 200 });
     } catch (error) {
         console.error(error);
 
